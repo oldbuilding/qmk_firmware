02cb4b98c05d0285fa1a7d5a99be0b35e793add7
Miguel Martin <mmartin@pcgus.com>
2024-04-04 18:42:13 -0400

    auto-update

--- keyboards/planck/keymaps/oldbuildling/keymap.c
+++ keyboards/planck/keymaps/oldbuildling/keymap.c
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  , KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -156,6 +156,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 set_single_persistent_default_layer(_DVORAK);
             }
             return false;
+            break;
         case BACKLIT:
             if (record->event.pressed) {
                 register_code(KC_RSFT);
@@ -163,6 +164,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_RSFT);
             }
             return false;
+            break;
         case PLOVER:
             if (record->event.pressed) {
                 layer_off(_RAISE);
@@ -177,11 +179,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 eeconfig_update_keymap(keymap_config.raw);
             }
             return false;
+            break;
         case EXT_PLV:
             if (record->event.pressed) {
                 layer_off(_PLOVER);
             }
             return false;
+            break;
         case PREV_WORD:
             if (record->event.pressed) {
                 if (os_mode == 0) {
@@ -193,7 +197,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
-        // other cases
+            break;
     }
     return true;
 }
